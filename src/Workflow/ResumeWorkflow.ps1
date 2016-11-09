fc55672a194d8b352c90b53c19dea0e0d72b8986
Filipe de Campos Cavalcante <filipe.cavalcante@programmers.com.br>
2016-11-09 12:51:39 -0200

    initial commit

---
+++ src/Workflow/ResumeWorkflow.ps1
@@ -0,0 +1,31 @@
+﻿Param(
+    # Url of Site
+    [Parameter(Mandatory=$true, Position=1)]
+    [string]$SiteUrl,
+
+    # Lists Title that contains Workflow
+    [Parameter(Mandatory=$true, Position=2)]
+    [string]$ListTitle,
+
+    # ID of Item
+    [Parameter(Mandatory=$true, Position=3)]
+    [int]$ItemId,
+
+    # Workflow Name
+    [Parameter(Mandatory=$true, Position=4)]
+    [string]$WorkflowInstanceName
+)
+try{
+    $site = Get-SPSite $SiteUrl
+    $web = $site.OpenWeb()
+    $list = $web.Lists[$ListTitle]
+    $item = $list.GetItemById($ItemId)
+    [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.WorkflowServicesBase")
+    $wf_mgr = New-Object -TypeName Microsoft.SharePoint.WorkflowServices.WorkflowServicesManager -ArgumentList $web
+    $wf_srv= $wf_mgr.GetWorkflowInstanceService()
+    $wfs = $wf_srv.EnumerateInstancesForListItem($list.ID, $item.ID) | Where-Object { $_.Name -eq "$WorkflowInstanceName"}
+    $wf_srv.ResumeWorkflow($wfs)
+}
+catch{
+    Write-Error -Exception $_.Exception.Message
+}
---
+++ src/Workflow/StartWorkflow.ps1
@@ -0,0 +1,37 @@
+﻿
+Param(
+    # Site Url
+    [Parameter(Mandatory=$true)]
+    [string]$sourceWebURL,
+
+    # List Title
+    [Parameter(Mandatory=$true)]
+    [string]$sourceListName,
+
+    [Parameter(Mandatory=$true)]
+    [string]$TargetWorkflow
+
+)
+
+$spSourceWeb = Get-SPWeb $sourceWebURL
+$spSourceList = $spSourceWeb.Lists[$sourceListName]
+ 
+#-- Getting a Workflow manager object to work with.
+$wfm = New-object Microsoft.SharePoint.WorkflowServices.WorkflowServicesManager($spSourceweb)
+#-- Getting the subscriptions
+$sub = $wfm.GetWorkflowSubscriptionService()
+#-- Getting the specific workflow within the list of subscriptions on the specific list. (SP2010 associated workflows basically)
+$WF = $sub.EnumerateSubscriptionsByList($spSourcelist.ID) | Where-Object {$_.Name -eq "$TargetWorkflow"}
+#-- Getting a Workflow instance in order to perform my commands.
+$wfis=$wfm.GetWorkflowInstanceService()
+
+Foreach($item in $spSourceList.Items){
+    $countStart++;
+    #-- Creating the dictionary object I need to parse into StartWorkflow. This could be most other workflow commands.
+    $object = New-Object 'system.collections.generic.dictionary[string,object]'
+    #--$object.Add("WorkflowStart", "StartWorkflow");
+    $wfRun = $wfis.StartWorkflowOnListItem($WF, $item.ID, $object)
+}
+
+
+#-- source: http://social.technet.microsoft.com/wiki/contents/articles/23850.sharepoint-2013-workflow-management-starting-a-workflow-using-powershell.aspx
